4572320a99b2a9b57bf01730a2bfa286c51c495d
SaiSadanand <73069784+SaiSadanand@users.noreply.github.com>
2021-05-27 14:31:26 +0530

    Add files via upload

---
+++ part1/1.1/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Total E={exercises1+exercises2+exercises3}/>
+    </div>  )
+}
+
+const Header=course=>{
+  return(
+    <div>
+      <h1>{course.course}</h1>
+    </div>
+  );
+}
+const Content=details=>{
+  return(
+    <div>
+      <p>
+        {details.p1} {details.e1}
+      </p>
+      <p>
+        {details.p2} {details.e2}
+      </p>
+      <p>
+        {details.p3} {details.e3}
+      </p>
+    </div>
+  )
+}
+
+const Total=total=>{
+  return(
+    <div>
+      <p>No:Of:Exercises:{total.E}</p>
+    </div>
+  )
+}
+
+export default App
