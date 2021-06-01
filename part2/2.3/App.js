c51b6623c580e9a1db41ec98d67e6b5d35d1242c
SaiSadanand <73069784+SaiSadanand@users.noreply.github.com>
2021-06-01 17:25:44 +0530

    Add files via upload

---
+++ part2/2.3/App.js
@@ -0,0 +1,85 @@
+import React from 'react'
+//import Note from './components/Note'
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id:1,
+    parts: [
+      {
+        id:1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id:2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id:3,
+        name: 'State of a component',
+        exercises: 14
+      },
+      {
+        id:4,
+        name: 'redux',
+        exercises: 11
+      }
+    ]
+  }
+  
+  
+
+  return (
+    <div>
+     <Course course={course}/>
+    </div>
+  )
+}
+
+const Course=({course})=>{
+  return(
+    <div>
+      <Header name={course.name}/>
+      <Content content={course.parts}/>
+      <Total content={course.parts}/>
+    </div>
+  )
+}
+
+const Header=({name})=>{
+  return(
+    <h2>{name}</h2>
+  )
+}
+
+const Content=({content})=>{
+  return(
+    <div>
+      {
+        content.map(con=>{return(
+            <Part key={con.id} c_name={con.name} exe={con.exercises}/>)
+          }
+        )
+      }
+    </div>
+  )
+}
+
+const Part=({c_name,exe})=>{
+  return(
+    <p>{c_name}  {exe}</p>
+  )
+}
+
+const Total=({content})=>{
+  const c=content.map((con)=>{return (con.exercises)})
+  const sum=c.reduce((accumilator,currentvalue)=>{return accumilator+currentvalue})
+  return(
+    <strong><p>total of {sum} exercises</p></strong>
+  )
+}
+
+export default App;
