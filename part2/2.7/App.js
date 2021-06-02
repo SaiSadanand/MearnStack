0c8cb301990e5f986f5cf790f044abe0ccfbd1b8
SaiSadanand <73069784+SaiSadanand@users.noreply.github.com>
2021-06-02 18:34:02 +0530

    Add files via upload

---
+++ part2/2.7/App.js
@@ -0,0 +1,53 @@
+import React,{useState} from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+    
+
+  const handleNameChange=(event)=>{
+      setNewName(event.target.value)
+  }
+
+  const addName=(event)=>{
+    event.preventDefault()
+    
+    if(persons.some((person)=>{return person.name===newName})===true)
+      {alert(`${newName} is already added to phoneBook`)}
+     else{ 
+        const person={
+          name:newName
+        }
+        setPersons(persons.concat(person))
+        setNewName('')
+      } 
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {
+          persons.map(p=>{return(
+            <h3 key={p.name}>{p.name}</h3>
+              )
+            }
+          )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
